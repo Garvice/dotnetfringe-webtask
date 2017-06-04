235dacc277b1b945fb21b0ee4dfefc225fcdd429
Geakins <garvice@geakins.com>
2017-06-04 10:43:42 -0700

    using import and arrow functions

--- wt-1.js
+++ wt-1.js
@@ -1,6 +1,8 @@
-var util = require('util');
+"use latest";
+import util from 'util';
 
-module.exports = function(ctx, cb) {
-  console.log(util.inspect(ctx.body, {depth:null}));
+module.exports = (ctx, cb) => {
+  // console.log(util.inspect(ctx.body, {depth:null}));
+  console.log(util.inspect(ctx.body.issue.url));
   cb();
 };
